53b72c25ab54a6657aa7c66711e4d6623832af0c
Mauricio Laine <laine.mau@gmail.com>
2021-05-25 21:06:25 -0500

    Show/Hide the button view if the talkback is enabled to avoid redundancy

--- app/src/main/java/com/laine/mauro/currencyrecognition/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/currencyrecognition/MainActivity.kt
@@ -2,6 +2,8 @@ package com.laine.mauro.currencyrecognition
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.accessibility.AccessibilityManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,9 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startButton.setOnClickListener {
+        titleImage.contentDescription = this.getString(R.string.start)
+        titleImage.setAccessibleButton()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setViews()
+    }
+
+    private fun setViews() {
+        val selectedView: View?
+        if (isTalkbackEnabled()) {
+            selectedView = titleImage
+            startButton.visibility = View.GONE
+        } else {
+            selectedView = startButton
+        }
+        selectedView?.setOnClickListener {
             val intent = Intent(this, RecognitionActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun isTalkbackEnabled(): Boolean {
+        val am = getSystemService(ACCESSIBILITY_SERVICE) as AccessibilityManager
+        return am.isEnabled && am.isTouchExplorationEnabled
     }
 }
